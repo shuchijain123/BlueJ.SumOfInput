8b0c228c454a2c6156df4fbef8b32c7cd91e8567
Shuchi Jain <shuchijain@zipcoders-MacBook-Pro.local>
2019-02-06 08:13:49 -0500

    committing sum of input

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please input your number");
+        int number = in.nextInt();
+        int sum=0;
+        
+        for(int i=1;i<=number;i++){
+        
+         sum = sum+i;
+         
+         
+        
 
     }
+    System.out.println("Total Sum till Number is " + sum);
+}
+    
 }
